8f3a58e9162e005b92141dd5b9298819b2c958e5
Alfredo Hernandez Baeza <alf_baza@hotmail.com>
2025-10-02 23:24:53 -0500

    feat: add Google Analytics script and .env.local example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {/* Google Analytics */}
+        {GA_TRACKING_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="gtag-init" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </>
+        )}
         <AuthProvider>
           <Header />
           {children}
